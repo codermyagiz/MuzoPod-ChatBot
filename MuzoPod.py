fecdc320c6d539fde92bc4e2fd42cf9917abab0b
musabkilic <musav70@gmail.com>
2018-06-17 16:44:28 +0300

    Add Python 2 Compatibility

--- MuzoPod.py
+++ MuzoPod.py
@@ -1,12 +1,25 @@
+# -*- coding: utf-8 -*-
 #encoding = utf8
 
 import speech_recognition as sr
 import pyaudio as audio
-from gtts import gTTS
 import os
+import sys
+import six
+from io import open
+from six.moves import input
+from gtts import gTTS
 
-name = input('İsmin Nedir?')
-qa = dict([x.split("\n") for x in open("tr.txt","r", encoding="utf8").read().split("\n\n")])
+if six.PY2:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+    from requests.packages import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+print(u"İsmin Nedir?")
+name = input()
+qa = dict([x.split("\n") for x in open("tr.txt","r", encoding="utf-8-sig").read().split("\n\n")])
 
 def speak(yazi, dil = "tr"):
     tts = gTTS(text=yazi, lang=dil) 
@@ -17,13 +30,16 @@ while True:
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print('Bir Şeyler Söyle')
+        print(u'Bir Şeyler Söyle')
         audio = r.listen(source)
         try:     
             estimate = r.recognize_google(audio, language="tr")
-            print('MuzoPod Sizin Şunu Söylediğinizi Düşünüyor :\n' + estimate)
+            print(u'MuzoPod Sizin Şunu Söylediğinizi Düşünüyor: ' + estimate)
             if estimate in qa.keys():
                 answer = qa[estimate].replace("{name}",name)
                 print(answer)
                 speak(answer)
-        except:pass
+        except KeyboardInterrupt:
+            break
+        except:
+            pass
